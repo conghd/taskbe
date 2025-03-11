282e66ce11e31b65c61d2af38fa205390f5ef041
Hoang Duc Cong <conghd@gmail.com>
2025-03-11 06:08:48 -0600

    Adding config for nodemon.

---
+++ server.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    apps: [
+      {
+        name: 'taskbe',
+        script: './server.js',
+        instances: 1,
+        exec_mode: 'fork',
+        watch: false, // If true, pm2 will restart when there is a file change, even log files
+        env: {
+          NODE_ENV: 'production',
+          PORT: '3000'
+        }
+      }
+    ]
+  };
